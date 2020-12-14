029dacaf72298dd0fc3a71bcabeb9725068454d3
DiniduRathnayaka <68413323+DiniduRathnayaka@users.noreply.github.com>
2020-12-14 22:30:11 +0530

    My first Angular

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -57,7 +57,8 @@ export class EmployeeComponent implements OnInit {
   onEdit( emp: Employee){
     this.employeeService.selectedEmployee = emp;
   }
-  onDelete( _id: string, form: NgForm){
+  onDelete( _id: string,
+     form: NgForm){
      if(confirm('Are you sure to delete this record ?')== true){
        this.employeeService.deleteEmployee(_id).subscribe((res)=>{
           this.refreshEmployeeList();
